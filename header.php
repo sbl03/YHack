0f4b0e5897d49c7072ce5bc830b4d76a27e3ebcf
sbl03 <sbl0323@gmail.com>
2013-11-08 21:32:48 -0500

    navbar

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Foundation 4</title>
+	
+	
+	<link rel="stylesheet" href="css/foundation.css">
+	
+	<script src="js/vender/jquery.js"></script>
+	<script src="js/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+
+<nav class="top-bar">
+	<ul class="title-area">
+		<li class="name">
+			<h1><a href="#">Title</a></h1>
+		</li>
+	</ul>
+	
+	<section class="top-bar-section">
+		<ul class="right">
+			<li><a href="#">Submit Synopsis</a></li>
+		</ul>
+	</section>
+</nav>
